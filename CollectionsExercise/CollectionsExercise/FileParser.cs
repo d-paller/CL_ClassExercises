ac175bd9fefc6e6865e2cafa99ace976ef3868b4
David Paller <darthdollar@gmail.com>
2018-09-25 18:59:51 -0400

    Update FileParser.cs
    Add comments

--- CollectionsExercise/CollectionsExercise/FileParser.cs
+++ CollectionsExercise/CollectionsExercise/FileParser.cs
@@ -11,22 +11,32 @@ namespace CollectionsExercise
     {
         public string[] Read(string path)
         {
+            // Initialize local variables
             int arraySize = 0;
             string[] array;
-
+            
+            // Create a new stream reader
             var reader = new StreamReader(path);
 
+            // Get the array size (how many rows of data are in the text file) from the file
             arraySize = int.Parse(reader.ReadLine());
 
+            // Create the array using the array size you got from the file
             array = new string[arraySize];
 
+            // Loop through the array giving it values
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = reader.ReadLine();
             }
+            
+            // Clean up
+            // Close the stream
             reader.Close();
+            // Use the built in Dispose method to properly dispose of the object
             reader.Dispose();
 
+            // Return the array with all of the data from the text file
             return array;
         }
     }
