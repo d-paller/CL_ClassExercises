41e4dd5f2cfbf9fff7da9996a93db47431b23f02
David Paller <dpaller94@gmail.com>
2019-01-23 13:14:11 -0500

    Add new exercise

---
+++ InterfacesAndStatics/InterfacesAndStatics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAndStatics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar mustang = new Car();
+            mustang.Drive();
+
+            // CarLot carlot = new CarLot(); // Error!
+
+            CarLot.Open();
+
+            Console.WriteLine("Car lot is open? " + CarLot.IsOpen);
+
+            CarLot.Close();
+        }
+    }
+}
